ebe303f206acb90d7662133653984138f8cdd8b7
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-06-27 12:02:56 +0900

    [AC] SubmissionID: 120703548 at 2021-06-27 12:02:56 (120703548)

---
+++ EZ Programming Contest #1/C_120703548_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/103150/submission/120703548
+ * Submitted at: 2021-06-27 12:02:56
+ * Problem URL: https://codeforces.com/contest/103150/problem/C
+ * Result: AC
+ * Execution Time: 46 ms
+ */
+
+// Could not fetch submission.
+// Perhaps it is private or deleted.
