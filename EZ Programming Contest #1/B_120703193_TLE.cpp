181125c3f6a1dcbe1f443371bffc2017c43b7fdd
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-06-27 11:48:18 +0900

    [TLE] SubmissionID: 120703193 at 2021-06-27 11:48:18 (120703193)

---
+++ EZ Programming Contest #1/B_120703193_TLE.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/103150/submission/120703193
+ * Submitted at: 2021-06-27 11:48:18
+ * Problem URL: https://codeforces.com/contest/103150/problem/B
+ * Result: TLE
+ * Execution Time: 1500 ms
+ */
+
+// Could not fetch submission.
+// Perhaps it is private or deleted.
