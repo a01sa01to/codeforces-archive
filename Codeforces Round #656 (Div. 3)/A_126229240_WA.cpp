1a1e9252791d418a55101fd14a96933d2d0f8a58
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-08-18 13:54:12 +0900

    [WA] SubmissionID: 126229240 at 2021-08-18 13:54:12 (126229240)

---
+++ Codeforces Round #656 (Div. 3)/A_126229240_WA.cpp
@@ -0,0 +1,51 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/1385/submission/126229240
+ * Submitted at: 2021-08-18 13:54:12
+ * Problem URL: https://codeforces.com/contest/1385/problem/A
+ * Result: WA
+ * Execution Time: 0 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> ll1d;
+typedef vector<ll1d> ll2d;
+
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+void solve() {
+  ll x, y, z;
+  cin >> x >> y >> z;
+  if (x == y == z) {
+    cout << "YES" << endl;
+    cout << x << " " << y << " " << z << endl;
+  }
+  else if (x == y && x > z) {  // aがmax
+    cout << "YES" << endl;
+    cout << x << " " << z << " " << z << endl;
+  }
+  else if (y == z && z > x) {  // cがmax
+    cout << "YES" << endl;
+    cout << x << " " << x << " " << y << endl;
+  }
+  else if (z == x && x > y) {  // bがmax
+    cout << "YES" << endl;
+    cout << y << " " << x << " " << y << endl;
+  }
+  else {
+    cout << "NO" << endl;
+  }
+}
+
+int main() {
+  cout << fixed << setprecision(15);
+  ll t;
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
