c4eaef2f4f3478cb2a696abbb51513a64aeca5b8
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-14 13:56:17 +0900

    [WA] SubmissionID: 128784151 at 2021-09-14 13:56:17 (128784151)

---
+++ Codeforces Round #582 (Div. 3)/D2_128784151_WA.cpp
@@ -0,0 +1,60 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/1213/submission/128784151
+ * Submitted at: 2021-09-14 13:56:17
+ * Problem URL: https://codeforces.com/contest/1213/problem/D2
+ * Result: WA
+ * Execution Time: 374 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<ll> ll1d;
+typedef vector<ll1d> ll2d;
+typedef pair<ll, ll> P;
+#define rep(i, n) for (ll i = 0; i < n; i++)
+#define all(vec) vec.begin(), vec.end()
+
+void solve() {
+  int n, k;
+  cin >> n >> k;
+  vector<priority_queue<int, vector<int>, greater<int>>> cnt(2e5 + 1);
+  rep(i, n) {
+    int a;
+    cin >> a;
+    int times = 0;
+    while (a > 0) {
+      cnt[a].push(times++);
+      a /= 2;
+    }
+    cnt[0].push(times);
+  }
+  int ans = 1e6;
+  rep(i, 2e5 + 1) {
+    if (cnt[i].size() >= k) {
+      int tmp = 0;
+      rep(_, k) {
+        int tp = cnt[i].top();
+        cnt[i].pop();
+        tmp += tp;
+      }
+      ans = min(ans, tmp);
+    }
+  }
+  cout << ans << endl;
+}
+
+int main() {
+  cout << fixed << setprecision(15);
+  // int t;
+  // cin >> t;
+  // while (t--) solve();
+  solve();
+  return 0;
+}
