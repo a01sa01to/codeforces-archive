1c6519a3502b9a1f022456685f736b165d464f51
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-15 13:22:33 +0900

    [WA] SubmissionID: 128867851 at 2021-09-15 13:22:33 (128867851)

---
+++ Codeforces Round #575 (Div. 3)/B_128867851_WA.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/1196/submission/128867851
+ * Submitted at: 2021-09-15 13:22:33
+ * Problem URL: https://codeforces.com/contest/1196/problem/B
+ * Result: WA
+ * Execution Time: 374 ms
+ */
+
+// Could not fetch submission.
+// Perhaps it is private or deleted.
