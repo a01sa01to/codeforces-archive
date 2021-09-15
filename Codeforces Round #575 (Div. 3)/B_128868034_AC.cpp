f7f68dbe45e6bd8574fe7a7765e453eb4a98ab46
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-15 13:26:13 +0900

    [AC] SubmissionID: 128868034 at 2021-09-15 13:26:13 (128868034)

---
+++ Codeforces Round #575 (Div. 3)/B_128868034_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/1196/submission/128868034
+ * Submitted at: 2021-09-15 13:26:13
+ * Problem URL: https://codeforces.com/contest/1196/problem/B
+ * Result: AC
+ * Execution Time: 1668 ms
+ */
+
+// Could not fetch submission.
+// Perhaps it is private or deleted.
