2c2c7443b34841d4fbf37d3fdc47a8144a3cacac
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-16 13:11:54 +0900

    [AC] SubmissionID: 128952066 at 2021-09-16 13:11:54 (128952066)

---
+++ Codeforces Round #570 (Div. 3)/A_128952066_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/1183/submission/128952066
+ * Submitted at: 2021-09-16 13:11:54
+ * Problem URL: https://codeforces.com/contest/1183/problem/A
+ * Result: AC
+ * Execution Time: 30 ms
+ */
+
+// Could not fetch submission.
+// Perhaps it is private or deleted.
