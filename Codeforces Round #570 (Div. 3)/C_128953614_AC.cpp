ecb535500e7426d56ea306c961b09225f3acafd7
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-16 13:50:15 +0900

    [AC] SubmissionID: 128953614 at 2021-09-16 13:50:15 (128953614)

---
+++ Codeforces Round #570 (Div. 3)/C_128953614_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/1183/submission/128953614
+ * Submitted at: 2021-09-16 13:50:15
+ * Problem URL: https://codeforces.com/contest/1183/problem/C
+ * Result: AC
+ * Execution Time: 982 ms
+ */
+
+// Could not fetch submission.
+// Perhaps it is private or deleted.
