124a629f9fbd7959004c1a15ff13f35806198781
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-09-16 13:11:37 +0900

    [WA] SubmissionID: 128952058 at 2021-09-16 13:11:37 (128952058)

---
+++ Codeforces Round #570 (Div. 3)/A_128952058_WA.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/1183/submission/128952058
+ * Submitted at: 2021-09-16 13:11:37
+ * Problem URL: https://codeforces.com/contest/1183/problem/A
+ * Result: WA
+ * Execution Time: 30 ms
+ */
+
+// Could not fetch submission.
+// Perhaps it is private or deleted.
