0c8c64cf98aafa8e50e39c00b96b94f839f6e24d
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-21 09:24:35 +0900

    [TLE] SubmissionID: 136295439 at 2021-11-21 09:24:35 (136295439)

---
+++ 2017-2018 ICPC Central Quarter Final of Northeastern European Regional Collegiate Programming Contest/J_136295439_TLE.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/102788/submission/136295439
+ * Submitted at: 2021-11-21 09:24:35
+ * Problem URL: https://codeforces.com/contest/102788/problem/J
+ * Result: TLE
+ * Execution Time: 1000 ms
+ */
+
+// Could not fetch submission.
+// Perhaps it is private or deleted.
