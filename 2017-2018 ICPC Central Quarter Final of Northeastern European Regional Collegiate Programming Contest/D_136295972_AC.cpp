fd13f5b39f7f54f3c911532d475178473e52d035
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-21 09:54:47 +0900

    [AC] SubmissionID: 136295972 at 2021-11-21 09:54:47 (136295972)

---
+++ 2017-2018 ICPC Central Quarter Final of Northeastern European Regional Collegiate Programming Contest/D_136295972_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/102788/submission/136295972
+ * Submitted at: 2021-11-21 09:54:47
+ * Problem URL: https://codeforces.com/contest/102788/problem/D
+ * Result: AC
+ * Execution Time: 186 ms
+ */
+
+// Could not fetch submission.
+// Perhaps it is private or deleted.
