a35ebc8b4e03e8acb9be32c3a9fb0d1e5d6215a0
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-28 09:40:43 +0900

    [AC] SubmissionID: 137167138 at 2021-11-28 09:40:43 (137167138)

---
+++ 2017-2018 ACM-ICPC Northern Eurasia (Northeastern European Regional) Contest (NEERC 17)/E_137167138_AC.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/101630/submission/137167138
+ * Submitted at: 2021-11-28 09:40:43
+ * Problem URL: https://codeforces.com/contest/101630/problem/E
+ * Result: AC
+ * Execution Time: 15 ms
+ */
+
+// Could not fetch submission.
+// Perhaps it is private or deleted.
