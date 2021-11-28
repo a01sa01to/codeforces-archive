3ccc59ad36da9455e92551eb0b0a772a1b2e03a2
Asa <26807394+a01sa01to@users.noreply.github.com>
2021-11-28 12:37:29 +0900

    [WA] SubmissionID: 137171152 at 2021-11-28 12:37:29 (137171152)

---
+++ 2017-2018 ACM-ICPC Northern Eurasia (Northeastern European Regional) Contest (NEERC 17)/A_137171152_WA.cpp
@@ -0,0 +1,11 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/101630/submission/137171152
+ * Submitted at: 2021-11-28 12:37:29
+ * Problem URL: https://codeforces.com/contest/101630/problem/A
+ * Result: WA
+ * Execution Time: 0 ms
+ */
+
+// Could not fetch submission.
+// Perhaps it is private or deleted.
