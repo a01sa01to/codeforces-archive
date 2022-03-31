cb6c7eaf95d40c394c54396d0b31bda2ecdb059f
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-04-01 00:39:33 +0900

    [AC] SubmissionID: 151559630 at 2022-04-01 00:39:33 (151559630)

---
+++ Codeforces Round #780 (Div. 3)/E_151559630_AC.cpp
@@ -0,0 +1,52 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/1660/submission/151559630
+ * Submitted at: 2022-04-01 00:39:33
+ * Problem URL: https://codeforces.com/contest/1660/problem/E
+ * Result: AC
+ * Execution Time: 592 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+void solve() {
+  int n;
+  cin >> n;
+  vector A(n, vector<int>(n));
+  int sum = 0;
+  rep(i, n) rep(j, n) {
+    char c;
+    cin >> c;
+    A[i][j] = c - '0';
+    sum += A[i][j];
+  }
+  int minim = 1e9;
+  rep(i, n) {
+    int cnt = 0;
+    rep(j, n) {
+      cnt += A[j][(i + j) % n];
+    }
+    minim = min(minim, (n - cnt) + (sum - cnt));
+  }
+  cout << minim << endl;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
