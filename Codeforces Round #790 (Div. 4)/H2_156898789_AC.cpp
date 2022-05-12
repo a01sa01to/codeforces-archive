70641b0542b2f2c86b42f832dbd32638efa41a8c
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-05-12 17:32:26 +0900

    [AC] SubmissionID: 156898789 at 2022-05-12 17:32:26 (156898789)

---
+++ Codeforces Round #790 (Div. 4)/H2_156898789_AC.cpp
@@ -0,0 +1,74 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/1676/submission/156898789
+ * Submitted at: 2022-05-12 17:32:26
+ * Problem URL: https://codeforces.com/contest/1676/problem/H2
+ * Result: AC
+ * Execution Time: 249 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+typedef long long ll;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+template<class T>
+struct fenwick_tree {
+  public:
+  fenwick_tree(): _n(0) {}
+  explicit fenwick_tree(int n): _n(n), data(n) {}
+
+  void add(int p, T x) {
+    assert(0 <= p && p < _n);
+    p++;
+    while (p <= _n) {
+      data[p - 1] += T(x);
+      p += p & -p;
+    }
+  }
+
+  T sum(int l, int r) {
+    assert(0 <= l && l <= r && r <= _n);
+    return sum(r) - sum(l);
+  }
+
+  private:
+  int _n;
+  std::vector<T> data;
+
+  T sum(int r) {
+    T s = 0;
+    while (r > 0) {
+      s += data[r - 1];
+      r -= r & -r;
+    }
+    return s;
+  }
+};
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep(i, n) cin >> a[i];
+  ll ans = 0;
+  fenwick_tree<ll> ft(n + 1);
+  rep(i, n) {
+    ans += ft.sum(a[i], n + 1);
+    ft.add(a[i], 1);
+  }
+  cout << ans << endl;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
