c7ff663dc4012f9ad4a6b8474054e106fb550015
Asa <26807394+a01sa01to@users.noreply.github.com>
2022-07-11 16:40:06 +0900

    [AC] SubmissionID: 163640021 at 2022-07-11 16:40:06 (163640021)

---
+++ Codeforces Round #805 (Div. 3)/F_163640021_AC.cpp
@@ -0,0 +1,70 @@
+/*
+ * Author: a01sa01to
+ * Submission URL: https://codeforces.com/contest/1702/submission/163640021
+ * Submitted at: 2022-07-11 16:40:06
+ * Problem URL: https://codeforces.com/contest/1702/problem/F
+ * Result: AC
+ * Execution Time: 1544 ms
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+  #include <atcoder/all>
+using namespace atcoder;
+#endif
+#ifdef LOCAL
+  #include "settings/debug.cpp"
+  #define _GLIBCXX_DEBUG
+#else
+  #define Debug(...) 0
+#endif
+using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+void solve() {
+  int n;
+  cin >> n;
+  multiset<int> a, b;
+  rep(i, n) {
+    int t;
+    cin >> t;
+    t /= (t & -t);
+    a.insert(t);
+  }
+  rep(i, n) {
+    int t;
+    cin >> t;
+    t /= (t & -t);
+    if (a.find(t) != a.end()) {
+      a.erase(a.find(t));
+    }
+    else {
+      b.insert(t);
+    }
+  }
+  Debug(a, b);
+  while (!a.empty() && !b.empty()) {
+    int x = *b.begin();
+    if (x == 0) {
+      b.erase(b.begin());
+    }
+    else if (a.find(x) != a.end()) {
+      a.erase(a.find(x));
+      b.erase(b.find(x));
+    }
+    else {
+      b.erase(b.find(x));
+      b.insert(x >> 1);
+    }
+    Debug(a, b);
+  }
+  cout << (a.empty() ? "YES" : "NO") << endl;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
